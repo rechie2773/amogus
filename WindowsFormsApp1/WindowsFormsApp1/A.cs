02f317539893031ae480d0f63fc1ac2498940eb8
Truong5613 <143669324+Truong5613@users.noreply.github.com>
2023-09-10 15:03:32 +0700

    Da them file A.cs

---
+++ WindowsFormsApp1/WindowsFormsApp1/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class A
+    {
+        private string NguyenHuyTruong;
+        private string DinhLeHuy;
+    }
+}
